77d393d8dd00002f059e0b8f126213a2c66b3c40
Egor Korshunov <sylaman@yandex.ru>
2026-02-01 11:05:45 +0300

    Add exceptions

--- game.py
+++ game.py
@@ -1,10 +1,22 @@
 from gameparts import Board
+from gameparts.exceptions import FieldIndexError
 
 
 def main():
     game = Board()
     game.display()
-    game.make_move(1, 1, 'X')
+    # Пользователь вводит номер строки.
+    row = int(input('Введите номер строки: '))
+    # Если введённое значение меньше нуля или больше или равно
+    # field_size (это значение равно трём, оно хранится в модуле
+    # parts.py)...
+    if row < 0 or row >= game.field_size:
+        # ...выбросить исключение FieldIndexError.
+        raise FieldIndexError
+    column = int(input('Введите номер столбца: '))
+    if column < 0 or column >= game.field_size:
+        raise FieldIndexError
+    game.make_move(row, column, 'X')
     print('Ход сделан!')
     game.display()
 
